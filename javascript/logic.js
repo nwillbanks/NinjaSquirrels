4153f57da07d40eccafa204bd347cf674fe44179
Pranab Rajbhandary <rajbhandaryp@gmail.com>
2018-11-07 00:22:44 -0600

    firebase for playlist

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,4 +1,3 @@
-/* firebase
 
   var config = {
     apiKey: "AIzaSyDnoIh_ls25YVOg3CkYj2B-OtheE3ds2gg",
@@ -10,10 +9,28 @@
   };
   firebase.initializeApp(config);
 
-  var database = firebase.database()
+
+var playlistId =[]
+var trackName =[]
+var artistName =[]
+var album =[]
+var year =[]
+var database = firebase.database()
+var a = '/connections_'+ Math.floor(Math.random()*1000)
+//console.log("random : " + a)
+var connectionRef = database.ref(a);
+var connectedRef =  database.ref(".info/connected");
 
 
-*/
+connectedRef.on("value", function(snap){
+  if (snap.val()){
+    var con = connectionRef.push(true);
+    //console.log("this is :" + connectedRef )
+    con.onDisconnect().remove()
+    
+  }
+})
+
 
 //grabbing the artist from html
 $("#add").on("click",function(event){
@@ -22,40 +39,41 @@ $("#add").on("click",function(event){
   var artist = $("#inputArtist").val()
   var song = $("#inputSong").val()
   var deezerApi; 
-
+  var flag = 0
   if (artist === "" && song === "" ){
-    console.log("no input")
+    // console.log("no input")
     
   }
   else if (artist === ""){
-    console.log("song only")
-     deezerApi = "https://api.deezer.com/search?q=track:"+ '"' + song +'"'
+    // console.log("song only")
+     deezerApi = "http://api.deezer.com/search?q=track:"+ '"' + song +'"'
+     flag = 1;
   }
   else if (song === ""){
-    console.log("artist only")
-     deezerApi = "https://api.deezer.com/search?q=artist:"+ '"' + artist +'"'
+    // console.log("artist only")
+     deezerApi = "http://api.deezer.com/search?q=artist:"+ '"' + artist +'"'
+     flag = 2;
   }
   else {
-    console.log("song & artist")
-    console.log("artist : " + artist)
-    console.log("song : " + song)
-   deezerApi = "https://api.deezer.com/search?q=artist:"+ '"' + artist +'"'  +" track:"+ '"' + song +'"' 
+    // console.log("song & artist")
+    // console.log("artist : " + artist)
+    // console.log("song : " + song)
+   deezerApi = "http://api.deezer.com/search?q=artist:"+ '"' + artist +'"'  +" track:"+ '"' + song +'"' 
+   flag = 3;
   }
- 
-  
-  console.log(deezerApi)
-  play(deezerApi)
-
-
+  //console.log(deezerApi)
+  play(deezerApi, flag)
 })
 
 
 // dezzer api
-function play(a){
+function play(a , flag){
   
   
   var deezerApi = a
   console.log("play: " + deezerApi)
+  var flag = flag;
+  console.log(flag)
 
   $.ajax({
     headers : {"Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS"},
@@ -64,11 +82,53 @@ function play(a){
 
   }).then(function(response){
     console.log(response)
-    var link = response.tracklist
-    console.log (link)
+    var result = response.data;
+    
+    for (var i = 0; i < 10; i++){
+      playlistId[i] = result[i].id
+      console.log ("he :" + playlistId[i])
+      trackName[i] = result[i].title
+      artistName[i] = result[i].artist.name
+      album[i] = result[i].album.title
+      writeUserData(playlistId[i],trackName[i],artistName[i],album[i])
+      //writeUserData(playlistId[i])
+      test();
+    }
+     // music()
+    console.log (playlistId)
+    //connectionRef.remove()
   })
 
 
 }
 
-//<iframe id='dzplayer' dztype='dzplayer' src='https://www.deezer.com/plugins/player?type=tracks&id=3135556&format=classic&color=007FEB&autoplay=false&playlist=true&width=700&height=240' scrolling='no' frameborder='0' style='border:none; overflow:hidden;' width='700' height='240' allowTransparency='true'></iframe>
+function music(){
+  $('#playDiv').append(
+    "<iframe id='dzplayer' dztype='dzplayer' src='https://www.deezer.com/plugins/player?type=tracks&id=" + playlistId[1] + " &format=classic&color=007FEB&autoplay=true&playlist=true&width=700&height=240' scrolling='no' frameborder='0' style='border:none; overflow:hidden;' width='700' height='240' allowTransparency='true'></iframe>"
+  )
+}
+
+
+function writeUserData(id,trackName,artist,album){
+  connectionRef.push({
+    id : id,
+    trackName :trackName,
+    artist : artist,
+    album : album,
+  })
+}
+
+function test() {
+  $("#values").empty()
+  connectionRef.on("child_added",function(snapshot){
+//need to add #values
+  $("#values").append(("<tr> " +
+   " <td> " + snapshot.val().id +" </td> "+
+   " <td> " + snapshot.val().trackName +" </td> "+
+   " <td> " + snapshot.val().artist +" </td> "+
+   " <td> " + snapshot.val().album +" </td> "+
+   " <td> " + snapshot.val().year + "</td> "));
+
+});
+
+}
